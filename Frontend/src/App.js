dce9427fcf9aca7ecacb513376aed3c9e30730c4
Vedant2104 <152850332+Vedant2104@users.noreply.github.com>
2024-09-24 21:53:14 +0530

    routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -14,28 +14,96 @@ import ErrorPage from './components/Error/error';
 import AdminDashboard from './components/Admin/AdminPage'; 
 import AboutUs from "./components/About-Us/aboutpage"
 import Info from './components/Nut_N_Rev/Info';
-
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+const serverURL = "http://localhost:5000"
 function App() {
   
+  const [Items,setItems] = useState([]);
+  // const [Kunal,setKunal] = useState([]);
+
+  useEffect(()=>{
+
+    const fetchItemCategories = async () => {
+      try {
+        const response = await axios.get(`${serverURL}/api/add-new/food-items`);
+        console.log(response.data);
+        setItems(response.data);
+      } catch (error) {
+        console.error('Error fetching item categories:', error);
+      }
+    };
+  
+    fetchItemCategories();
+  },[])
+
   return (
     <CartProvider>
       <Router>
         <div className="main-body">
           <NavBar />
           <Routes>
-            <Route exact path="/" element={<><Home /></>} />
-            <Route exact path="/menu" element={<><MenuPage /></>} />
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <Home />
+                </>
+              }
+            />
+            <Route
+              exact
+              path="/menu"
+              element={
+                <>
+                  <MenuPage />
+                </>
+              }
+            />
             <Route path="/sign-in" element={<Login />} />
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/profile" element={<UserProfile />} />
             <Route path="/cart" element={<ShoppingCart />} />
 
+            <Route
+              path="/admin/"
+              element={
+                <>
+                  <AdminDashboard />
+                </>
+              }
+            />
+            <Route
+              path="/admin/add-items"
+              element={
+                <>
+                  <AddItem />
+                </>
+              }
+            />
+            <Route
+              path="/about-us"
+              element={
+                <>
+                  <AboutUs />
+                </>
+              }
+            />
+            {/* <Route path="/info" element={<><Info/></>} /> Rajbir mat hataiyo */}
+            {Items.map((things) =>
+              things.food_item.map((item) => (
+                <Route
+                  path={`/info/${item.id}`}
+                  element={
+                    <>
+                      <FoodItem item={item} />
+                    </>
+                  }
+                />
+              ))
+            )}
 
-
-            <Route path="/admin/" element={<><AdminDashboard /><Footer /></>} />
-            <Route path="/admin/add-items" element={<><AddItem /><Footer /></>} />
-            <Route path="/about-us" element={<><AboutUs /><Footer /></>} />
-            
             <Route path="*" element={<ErrorPage />} />
           </Routes>
           <Footer />
